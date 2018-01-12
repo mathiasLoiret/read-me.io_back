d7bbcbe2b0cab02f2e70a3021073ff4d3e103154
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-12 18:15:19 +0100

    Init superagent test

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.send('Hello Ynov');
+});
+
+app.get('/api/health', function(req, res) {
+  res.sendStatus(200);
+});
+
+module.exports = app
