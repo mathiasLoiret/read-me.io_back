37251f7a74f411bc2deb2e944ac035b3c00de628
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-01-12 09:21:02 +0100

    Init basic Express web app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello Ynov')
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get('/', function(req, res) {
+  res.send('Hello Ynov');
+});
+
+app.listen(port, function() {
+  console.log(`Server started on port ${port}!`);
+})
