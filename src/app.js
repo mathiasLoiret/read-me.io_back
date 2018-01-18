e96c576024829b0569c504dbc1639a1001b5dcd4
Steven Fongue <sfongue@gmail.com>
2018-01-18 12:16:53 +0100

    add api extensions and templates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+const readline =require('readline');
 
 app.use(express.static('public'));
 
@@ -10,5 +12,34 @@ app.get('/', function(req, res) {
 app.get('/api/health', function(req, res) {
   res.sendStatus(200);
 });
+
+app.get('/api/extensions', function(req, res) {
+  fs.readdir('./src/templates/', function(err, content) {
+    if(err) {
+      res.sendStatus(500);
+    }
+    var listExtensions = [];
+    for(var i=0; i<content.length; i++) {
+      listExtensions.push(content[i]);
+    }
+    var extension = { extensions: listExtensions }
+    res.send(JSON.stringify(extension));
+  })
+})
+
+app.get('/api/templates', function(req, res) {
+  fs.readdir('./src/templates/markdown/', function(err, contents) {
+    if(err) {
+      res.sendStatus(500);
+    }
+    var listTemplates = [];
+    for(var i=0; i<contents.length; i++) {
+      var content = contents[i].split(".")
+      listTemplates.push(content[0]);
+    }
+    var template = { templates: listTemplates}
+    res.send(JSON.stringify(template));
+  })
+})
 
 module.exports = app
