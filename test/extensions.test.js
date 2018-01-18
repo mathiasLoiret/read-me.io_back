e987dc4c9f746e79b71ef5c786ccb837bcb51d82
Killy85 <goureau.n.a@hotmail.fr>
2018-01-18 10:49:31 +0100

    Adding test files for extensions and templates routes. Skipped for the moment

---
+++ test/extensions.test.js
@@ -0,0 +1,32 @@
+const superagent = require('superagent');
+const expect = require('chai').expect;
+
+let host = 'http://localhost:3000';
+let urlAPI = '/'
+
+describe('Project Root --> /', function() {
+
+  let app = require('../src/app.js');
+  let instance;
+
+  before(function() {
+    instance = app.listen(3000);
+  });
+  after(function() {
+    instance.close();
+  });
+
+  describe('GET', function() {
+    it(`GIVEN ${urlAPI}
+        WHEN send GET request
+        THEN should return 'Hello Ynov'`, function(done) {
+      superagent.get(`${host}${urlAPI}`)
+        .end(function(e, res) {
+          expect(res.status).to.eql(200)
+          expect(res.text).to.equal("{['asciidoc','txt','markdown']}")
+          done()
+        })
+    })
+  }).skip()
+
+})
