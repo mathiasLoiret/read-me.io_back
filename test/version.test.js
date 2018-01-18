37f2ed500bf4ce39c5909264605ceb7dc4e1aa67
Matthieu Fournier <mat.fournier85@gmail.com>
2018-01-18 16:12:54 +0100

    add test route version

---
+++ test/version.test.js
@@ -0,0 +1,32 @@
+const superagent = require('superagent');
+const expect = require('chai').expect;
+
+let host = 'http://localhost:3000';
+let urlAPI = '/api/version'
+
+describe('Project Root --> /', function() {
+
+  let app = require('../src/app.js');
+  let instance;
+
+  before(function() {
+    instance = app.listen(3000);
+  });
+  after(function() {
+    instance.close();
+  });
+
+  describe('GET', function() {
+    it(`GIVEN ${urlAPI}
+        WHEN send GET request
+        THEN should return a JSON object with the version attribute `, function(done) {
+      superagent.get(`${host}${urlAPI}`)
+        .end(function(e, res) {
+          expect(res.status).to.eql(200)
+          expect(JSON.parse(res.text)).to.have.property('version')
+          done()
+        })
+    })
+  })
+
+})
