4390706268132aa11a8924d2c24255719faa3088
Killy85 <goureau.n.a@hotmail.fr>
2018-01-19 13:26:41 +0100

    Changing strategy for the production script. Using gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+
+var paths = {
+  src: 'src/*',
+  index: 'index.js',
+  packa: 'package.json',
+  publi:'public/*',
+  dist: 'dist'
+};
+
+
+gulp.task('default', function () {
+  console.log('Hello World!');
+});
+
+gulp.task('src', function () {
+  return gulp.src(paths.src).pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('index', function () {
+  return gulp.src(paths.index).pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('package', function () {
+  return gulp.src(paths.packa).pipe(gulp.dest(paths.dist));
+});
+gulp.task('publi', function () {
+  return gulp.src(paths.publi).pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task('copy', ['src', 'index', 'package', 'publi']);
--- production.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "dist/" ]; then
-  rm -rf dist/
-fi
-mkdir dist
-cp index.js dist/index.js
-cp package.json dist/package.json
-cp -r public dist/public
-cp -r src dist/src
