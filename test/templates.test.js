70f04345d3590d4781d42ed5a34a366eb461086b
Killy85 <goureau.n.a@hotmail.fr>
2018-01-19 16:26:20 +0100

    Merging with orlando's. Update of python template

--- test/templates.test.js
+++ test/templates.test.js
@@ -1,8 +1,12 @@
 const superagent = require('superagent');
 const expect = require('chai').expect;
+const before  = require('mocha').before;
+const after  = require('mocha').after;
+const describe  = require('mocha').describe;
+const it = require('mocha').it;
 
 let host = 'http://localhost:3000';
-let urlAPI = '/api/templates'
+let urlAPI = '/api/templates';
 
 describe('Project Root --> /', function() {
 
@@ -17,23 +21,12 @@ describe('Project Root --> /', function() {
   });
 
   describe('GET', function() {
-    it(`GIVEN ${urlAPI}
-        WHEN send GET request
-        THEN a list of availables templates`, function(done) {
-      superagent.get(`${host}${urlAPI}`)
-        .end(function(e, res) {
-          expect(res.status).to.eql(200)
-          expect(JSON.parse(res.text)).to.eql({
-            templates: [
-              'basic',
-              'java',
-              'node',
-              'python'
-            ]
-          })
-          done()
-        })
-    })
-  })
-
-})
+    it(`GIVEN ${urlAPI} WHEN send GET request THEN a list of availables templates`, function(done) {
+      superagent.get(`${host}${urlAPI}`).end(function(e, res) {
+        expect(res.status).to.eql(200);
+        expect(JSON.parse(res.text)).to.eql({templates:['basic','csharp','java','node','python']});
+        done();
+      });
+    });
+  });
+});
