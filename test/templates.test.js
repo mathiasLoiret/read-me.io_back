f29e2c562fd1ea911e173f7ba5f97980d2be48d7
Killy85 <goureau.n.a@hotmail.fr>
2018-01-19 16:26:20 +0100

    Merging with orlando's. Update of python template

--- test/templates.test.js
+++ test/templates.test.js
@@ -21,24 +21,12 @@ describe('Project Root --> /', function() {
   });
 
   describe('GET', function() {
-    it(`GIVEN ${urlAPI}
-        WHEN send GET request
-        THEN a list of availables templates`, function(done) {
-      superagent.get(`${host}${urlAPI}`)
-        .end(function(e, res) {
-          expect(res.status).to.eql(200)
-          expect(JSON.parse(res.text)).to.eql({
-            templates: [
-              'basic',
-              'csharp',
-              'java',
-              'node',
-              'python'
-            ]
-          })
-          done()
-        })
-    })
-  })
-
-})
+    it(`GIVEN ${urlAPI} WHEN send GET request THEN a list of availables templates`, function(done) {
+      superagent.get(`${host}${urlAPI}`).end(function(e, res) {
+        expect(res.status).to.eql(200);
+        expect(JSON.parse(res.text)).to.eql({templates:['basic','csharp','java','node','python']});
+        done();
+      });
+    });
+  });
+});
