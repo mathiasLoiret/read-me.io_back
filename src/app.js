4f7fabb378ff8ecc41ea92dca7cc10fa2d783461
Killy85 <goureau.n.a@hotmail.fr>
2018-01-19 16:35:08 +0100

    We did lint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const readline =require('readline');
-const templatePath = "src/templates"
+const templatePath = 'src/templates';
 const pjson = require('./../package.json');
 
 var tempList = ['basic'];
-getTemplates((err, templates)=>{if(!err){tempList = templates}})
+getTemplates((err, templates)=>{if(!err){tempList = templates;}});
 var extList = ['asciidoc'];
-getExtentions((err, extensions)=>{if(!err){extList = extensions}})
+getExtentions((err, extensions)=>{if(!err){extList = extensions;}});
 
 app.use(express.static('public'));
 
@@ -22,35 +21,35 @@ app.get('/api/health', function(req, res) {
 
 app.get('/api/version', function(req, res) {
   res.status(200);
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ version: pjson.version }))
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ version: pjson.version }));
 });
 
 app.get('/api/extensions', function(req, res) {
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Type', 'application/json');
   getExtentions((err, listExtensions) => {
-  	if(err){
-  		res.sendStatus(500)
-  		res.end(JSON.stringify(err))
-  	}else{
-  		res.status(200);
-  		res.send(JSON.stringify({ extensions: listExtensions}));
-  	}
-  })
-})
+    if(err){
+      res.sendStatus(500);
+      res.end(JSON.stringify(err));
+    }else{
+      res.status(200);
+      res.send(JSON.stringify({ extensions: listExtensions}));
+    }
+  });
+});
 
 app.get('/api/templates', function(req, res) {
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Type', 'application/json');
   getTemplates((err, listTemplates)=>{
-  	if(err){
-  		res.status(500);
-  		res.end(JSON.stringify(err))
-  	}else{
-  		res.status(200);
-  		res.send(JSON.stringify({ templates: listTemplates}));
-  	}
-  })
-})
+    if(err){
+      res.status(500);
+      res.end(JSON.stringify(err));
+    }else{
+      res.status(200);
+      res.send(JSON.stringify({ templates: listTemplates}));
+    }
+  });
+});
 
 app.get('/api/generate', function(req, res) {
   res.status(200);
@@ -60,17 +59,17 @@ app.get('/api/generate', function(req, res) {
       res.status(404);
       res.end(JSON.stringify(err));
     }else{
-      res.end(JSON.stringify(resContent))
+      res.end(JSON.stringify(resContent));
     }
 
-  })
+  });
 });
 
 function generate(data, callback){
-	var resObj = {}
+  var resObj = {};
   try {
     resObj['template'] = getTemplate(data.temp);
-  	resObj['ext'] = getExtention(data.ext);
+    resObj['ext'] = getExtention(data.ext);
 
     getfile(`${resObj['ext']}/${resObj['template']}.${resObj['ext']}`, (err, fileContent) => {
       if(err){
@@ -79,7 +78,7 @@ function generate(data, callback){
         resObj['file'] = fileContent;
         callback(undefined,resObj);
       }
-  	});
+    });
 
   } catch (e) {
     callback(e, undefined);
@@ -87,59 +86,59 @@ function generate(data, callback){
 }
 
 function getfile(filePath, callback){
-   fs.readFile(`${templatePath}/${filePath}`, (err, data) =>{
-      if(err){
-        callback(err, undefined);
-      }else{
-        callback(undefined, data.toString());
-      }
-    });
+  fs.readFile(`${templatePath}/${filePath}`, (err, data) =>{
+    if(err){
+      callback(err, undefined);
+    }else{
+      callback(undefined, data.toString());
+    }
+  });
 }
 
 function getTemplates(callback){
   fs.readdir('./src/templates/markdown/', function(err, contents) {
     if(err) {
-      callback(err, undefined)
+      callback(err, undefined);
     }else{
-    	var listTemplates = [];
-	    for(var i=0; i<contents.length; i++) {
-	      listTemplates.push(contents[i].split(".")[0]);
-	    }
-	    callback(undefined, listTemplates );
+      var listTemplates = [];
+      for(var i=0; i<contents.length; i++) {
+        listTemplates.push(contents[i].split('.')[0]);
+      }
+      callback(undefined, listTemplates );
     }
-  })
+  });
 }
 
 function getTemplate(value){
-	if(value == undefined){
-		return 'basic';
-	}else if(tempList.indexOf(value) != -1 ){
-		return value;
-	}
-	throw {err:'unreconized template'}
+  if(value == undefined){
+    return 'basic';
+  }else if(tempList.indexOf(value) != -1 ){
+    return value;
+  }
+  throw {err:'unreconized template'};
 }
 
 function getExtentions(callback){
-	fs.readdir('./src/templates/', function(err, content) {
-		if(err) {
-			callback(err, undefined)
-		}else{
-			var listExtensions = [];
-			for(var i=0; i<content.length; i++) {
-				listExtensions.push(content[i]);
-			}
-			callback(undefined, listExtensions)
-		}
-	})
+  fs.readdir('./src/templates/', function(err, content) {
+    if(err) {
+      callback(err, undefined);
+    }else{
+      var listExtensions = [];
+      for(var i=0; i<content.length; i++) {
+        listExtensions.push(content[i]);
+      }
+      callback(undefined, listExtensions);
+    }
+  });
 }
 
 function getExtention(value){
-	if(value == undefined){
-		return 'asciidoc';
-	}else if(extList.indexOf(value) != -1 ){
-		return value;
-	}
-	throw {err:'unreconized extension'};
+  if(value == undefined){
+    return 'asciidoc';
+  }else if(extList.indexOf(value) != -1 ){
+    return value;
+  }
+  throw {err:'unreconized extension'};
 }
 
-module.exports = app
+module.exports = app;
